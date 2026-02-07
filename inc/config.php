d59a6446a86cb5a0a90cbf53373f2fa93d19ae66
Faudead <neruer@gmail.com>
2026-02-07 16:04:55 +0200

    init: project scaffold

---
+++ inc/config.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+return [
+  'db' => [
+    'host' => 'localhost',
+    'name' => 'DB_NAME',
+    'user' => 'DB_USER',
+    'pass' => 'DB_PASS',
+    'charset' => 'utf8mb4',
+  ],
+  'app' => [
+    'session_name' => 'CHARAPPSESS',
+  ],
+];
+
